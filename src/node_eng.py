0f6fb847b66cf67902c80d3b329ac7a3d525ff28
Eddie <ehwa009@aucklanduni.ac.nz>
2018-12-13 00:46:52 +1300

    add eng and kor version

--- src/node_eng.py
+++ src/node_eng.py
@@ -8,16 +8,18 @@ import json
 import random
 
 from boto3.dynamodb.conditions import Key, Attr
-from mind_msgs.msg import Reply, RaisingEvents, DBQuery
-from mind_msgs.srv import ReloadWithResult, ReadData, WriteData, GoogleEntity
+from mind_msgs.srv import DBQuery
 
 LOCATION_TEXT = [
     "The {location} is {desc}.",
 ]
-PRESCRIPTION_TEXT = [
+PRESCRIPTION_TEXT_FAIL = [
     '''I'm sorry {name}, your doctor has not yet written your prescription, and so it is not ready for collection at the moment.
     However, I have sent a message to your doctor. Once the prescription has been written, someone will call you and let you know.
     '''
+]
+PRESCRIPTION_TEXT_SUCCESS = [
+    "Great! Here it is.", "Ok, Here it is."
 ]
 FORGOT_DR_NAME = [
     "No problem {name}, I can see that you have an appointment with {dr_name} today and have checked you in"
@@ -46,58 +48,45 @@ class DynamoDBNode:
         self.table = dynamodb.Table(data['table'])
         # print(self.table.creation_date_time)
 
-        self.pub_reply = rospy.Publisher('reply', Reply, queue_size=10)
+        self.srv_query_db = rospy.Service('%s/query_data'%rospy.get_name(), DBQuery, self.handle_api_call)
 
-        rospy.Subscriber('query', DBQuery, self.handle_api_call)
         rospy.loginfo('%s initialized' %rospy.get_name())
         rospy.spin()
 
     
-    def handle_api_call(self, msg):
-        api_call = msg.api_number
-        entities = json.loads(msg.entities)
-
-        if api_call == 3:
-            # resp = self.get_data(entities['<location>'])
-            resp = self.get_data('bathroom')
-
+    def handle_api_call(self, data):
+        data = data.api_call
+        res = data.split(' ')
+        if res[1] == 'appointment':
+            name = res[2] + ' ' + res[3]
+            address = res[4] + ' ' + res[5] + ' ' + res[6]
+            resp = self.get_data_with_attr(name, address)
+            output_string = random.choice(FORGOT_DR_NAME)
+            response = output_string.format(name=resp['Name'], dr_name=resp['DrName'])
+        elif res[1] == 'location':
+            resp = self.get_data(res[2])
             output_string = random.choice(LOCATION_TEXT)
             response = output_string.format(location=resp['Name'], desc=resp['Desc'])
+        elif res[1] == 'prescription':
+            name = res[2] + ' ' + res[3]
+            address = res[4] + ' ' + res[5] + ' ' + res[6]
+            resp = self.get_data_with_attr(name, address)
+            if resp['Prescription'] is False:
+                output_string = random.choice(PRESCRIPTION_TEXT_FAIL)
+                response = output_string.format(name=resp['Name'])
+            else:
+                response = random.choice(PRESCRIPTION_TEXT_SUCCESS)
+        elif res[1] == 'waiting_time':
+            name = res[2] + ' ' + res[3]
+            address = res[4] + ' ' + res[5] + ' ' + res[6]
+            resp = self.get_data_with_attr(name, address)
+            dr_info = self.get_data(resp['DrName'])
+            output_string = random.choice(WAITING_TIME)
+            response = output_string.format(dr_name=dr_info['Name'], waiting_time=dr_info['WaitingTime'])
         else:
-            resp = self.get_data_with_attr(entities['<patient_name>'], entities['<address>'])
-            if api_call == 4:
-                if resp['Prescription'] is False:
-                    output_string = random.choice(PRESCRIPTION_TEXT)
-                    response = output_string.format(name=resp['Name'])
-                else:
-                    response = "Ok, here you go."
-            elif api_call == 2:
-                # print(resp['Time'])
-                # print(entities['<time>'])
-                # if resp['Time'].lower() == entities['<time>']:
-                try:
-                    output_string = random.choice(FORGOT_DR_NAME)
-                    response = output_string.format(name=resp['Name'], dr_name=resp['DrName'])
-                except:
-                    response = 'Something wrong.'
-            elif api_call == 1:
-                dr_info = self.get_data(resp['DrName'])
-                output_string = random.choice(WAITING_TIME)
-                response = output_string.format(dr_name=dr_info['Name'], waiting_time=dr_info['WaitingTime'])
-                
-        reply_msg = Reply()
-        reply_msg.header.stamp = rospy.Time.now()
-        reply_msg.reply = response
+            print('something wrong.')
 
-        self.pub_reply.publish(reply_msg)
-
-    # def get_data(self, key):
-    #     resp = self.table.get_item(
-    #                 Key={
-    #                     'Name': key
-    #                 }
-    #             )
-    #     return resp['Item']
+        return response
 
     def get_data(self, key):
         resp = self.table.query(
@@ -115,4 +104,3 @@ class DynamoDBNode:
 if __name__ == '__main__':
     rospy.init_node('dynamodb_node', anonymous=False)
     db = DynamoDBNode()
-    
---
+++ src/node_kor.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python
+#-*- encoding: utf8 -*-
+
+import rospy
+import boto3
+import yaml
+import json
+import random
+
+from boto3.dynamodb.conditions import Key, Attr
+from mind_msgs.srv import DBQuery
+
+LOCATION_TEXT = [
+    u"네, {location}은 {desc}에 있어요.",
+]
+PRESCRIPTION_TEXT_FAIL = [
+    u"죄송해요 {name}님, 아직 의사선생님께서 처방전을 작성하지 않아서 아직 준비가 안됬어요. 제가 의사선생님께 이를 알려드렸어요. 처방전이 준비 되었을때, 알려드리도록 할게요."
+]
+PRESCRIPTION_TEXT_SUCCESS = [
+    u"잠시만 기다리세요. 여기 있습니다.", "여기 있습니다."
+]
+FORGOT_DR_NAME = [
+    u"걱정 마세요 {name}님, 오늘 {dr_name}의사 선생님과 예약이 잡혀있어요. 접수 해 드렸습니다."
+]
+WAITING_TIME = [
+    u"네, {dr_name}의사 선생님은 {waiting_time} 분 뒤에 진료 가능하십니다."
+]
+
+
+class DynamoDBNode:
+    
+    def __init__(self):
+        try:
+            config_file = rospy.get_param('~config_file')
+        except:
+            rospy.logerr('please set param ~config_file.')
+            exit(-1)
+
+        with open(config_file) as f:
+            data = yaml.load(f)
+        dynamodb = boto3.resource('dynamodb',
+            region_name=data['region'],
+            aws_access_key_id=data['aws_access_key_id'],
+            aws_secret_access_key=data['aws_secret_access_key'])
+
+        self.table = dynamodb.Table(data['table'])
+        # print(self.table.creation_date_time)
+
+        self.srv_query_db = rospy.Service('%s/query_data'%rospy.get_name(), DBQuery, self.handle_api_call)
+
+        rospy.loginfo('%s initialized' %rospy.get_name())
+        rospy.spin()
+
+    
+    def handle_api_call(self, data):
+        data = data.api_call
+        res = data.split(' ')
+        if res[1] == '예약':
+            name = res[2]
+            address = res[3]
+            resp = self.get_data_with_attr(name, address)
+            output_string = random.choice(FORGOT_DR_NAME)
+            response = output_string.format(unicode(name=resp['Name']), unicode(dr_name=resp['DrName']))
+        elif res[1] == '위치':
+            resp = self.get_data(res[2])
+            output_string = random.choice(LOCATION_TEXT)
+            response = output_string.format(location=resp['Name'], desc=resp['Desc'])
+        elif res[1] == '처방전':
+            name = res[2]
+            address = res[3]
+            resp = self.get_data_with_attr(name, address)
+            if resp['Prescription'] is False:
+                output_string = random.choice(PRESCRIPTION_TEXT_FAIL)
+                response = output_string.format(name=resp['Name'])
+            else:
+                response = random.choice(PRESCRIPTION_TEXT_SUCCESS)
+        elif res[1] == '대기시간':
+            name = res[2]
+            address = res[3]
+            resp = self.get_data_with_attr(name, address)
+            dr_info = self.get_data(resp['DrName'])
+            output_string = random.choice(WAITING_TIME)
+            response = output_string.format(dr_name=dr_info['Name'], waiting_time=dr_info['WaitingTime'])
+        else:
+            print('something wrong.')
+
+        return response
+
+    def get_data(self, key):
+        resp = self.table.query(
+            KeyConditionExpression=Key('Name').eq(key)
+        )
+        return resp['Items'][0]
+
+    def get_data_with_attr(self, key, attr):
+        resp = self.table.scan(
+            FilterExpression=Attr('Name').eq(key) & Attr('Address').eq(attr)
+        )
+        return resp['Items'][0]
+
+
+if __name__ == '__main__':
+    rospy.init_node('dynamodb_node', anonymous=False)
+    db = DynamoDBNode()
